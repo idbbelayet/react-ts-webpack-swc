7516e388c0b7cbc3d386a73c658465509e72c515
Mohammad Belayet Hossain <alnoorkg@gmail.com>
2025-09-06 23:46:42 +0600

    redux form remove and react final form added

--- src/pages/ReduxForm.tsx
+++ src/pages/ReduxForm.tsx
@@ -1,6 +1,6 @@
 import { Button, Dropdown, Label, Option, Radio, Textarea } from '@fluentui/react-components';
 import { DatePicker } from '@fluentui/react-datepicker-compat';
-import { Field, reduxForm } from 'redux-form';
+import { Field, Form } from 'react-final-form';
 import EmployeeInfo from '../models/employeeInfo';
 import React from 'react';
 import TextField from '../components/controls/TextField';
@@ -10,7 +10,6 @@ const sleep = (ms: any) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const ReduxForm = (props: any) => {
   const [result, setResult] = React.useState<any>({});
-  const { handleSubmit, pristine, reset, submitting } = props;
 
   async function showResults(values: EmployeeInfo) {
     await sleep(500); // simulate server latency
@@ -20,116 +19,137 @@ const ReduxForm = (props: any) => {
   return (
     <div className="flex flex-col items-start justify-start p-6">
       <div className="mb-5 text-2xl">Redux Form with Fluent UI</div>
-      <form onSubmit={handleSubmit(showResults)}>
-        <div className="flex flex-col gap-4">
-          <Field
-            name="fullName"
-            component={TextField}
-            type="text"
-            placeholder="Full Name"
-            label="Full Name"
-            required
-          />
-
-          <Field name="email" component={TextField} type="text" placeholder="Email" label="Email" />
-
-          <div>
-            <Label>Sex</Label>
-            <div>
-              <Label>
-                <Field name="sex" component={Radio} type="radio" value="male" label="Male" />
-              </Label>
-              <Label>
-                <Field name="sex" component={Radio} type="radio" value="female" label="Female" />
-              </Label>
-            </div>
-          </div>
-
-          <Field name="age" component={TextField} type="number" placeholder="Asge" label="Age" />
-
-          <div>
-            <Label>Favorite Color</Label>
-            <div>
+      <Form
+        onSubmit={showResults}
+        render={({ handleSubmit, submitting, pristine }) => (
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-col gap-4">
               <Field
-                name="favoriteColor"
-                id="favoriteColor"
-                component={Dropdown}
-                multiselect={false}
-                clearable
-                placeholder="Select"
-              >
-                <Option value="1">Action 1</Option>
-                <Option value="2">Action 2 </Option>
-                <Option value="3">Action 3</Option>
-              </Field>
-            </div>
-          </div>
-          <div>
-            <Label>Skills</Label>
-            <div>
-              <Field
-                name="skills"
-                id="skills"
-                component={Dropdown}
-                multiselect={true}
-                clearable
-                placeholder="Select"
-              >
-                <Option value="1">.Net Core</Option>
-                <Option value="2">React js </Option>
-                <Option value="3">Next js</Option>
-                <Option value="4">Angular js</Option>
-              </Field>
-            </div>
-          </div>
-          <Field
-            name="isActive"
-            id="isActive"
-            component={CheckboxField}
-            label="Is Active"
-            type="checkbox"
-          />
-
-          <div>
-            <Label htmlFor="joiningDate">Joining Date</Label>
-            <div>
-              <Field
-                name="joiningDate"
-                id="joiningDate"
-                component={DatePicker}
-                placeholder="Joining Date"
-                maxDate={new Date()}
-                isMonthPickerVisible={false}
+                name="fullName"
+                component={TextField}
+                type="text"
+                placeholder="Full Name"
+                label="Full Name"
+                required
               />
-            </div>
-          </div>
 
-          <div>
-            <Label>Notes</Label>
-            <div>
-              <Field name="notes" id="notes" component={Textarea} />
+              <Field
+                name="email"
+                component={TextField}
+                type="text"
+                placeholder="Email"
+                label="Email"
+              />
+
+              <div>
+                <Label>Sex</Label>
+                <div>
+                  <Label>
+                    <Field name="sex" component={Radio} type="radio" value="male" label="Male" />
+                  </Label>
+                  <Label>
+                    <Field
+                      name="sex"
+                      component={Radio}
+                      type="radio"
+                      value="female"
+                      label="Female"
+                    />
+                  </Label>
+                </div>
+              </div>
+
+              <Field
+                name="age"
+                component={TextField}
+                type="number"
+                placeholder="Asge"
+                label="Age"
+              />
+
+              <div>
+                <Label>Favorite Color</Label>
+                <div>
+                  <Field
+                    name="favoriteColor"
+                    id="favoriteColor"
+                    component={Dropdown}
+                    multiselect={false}
+                    clearable
+                    placeholder="Select"
+                  >
+                    <Option value="1">Action 1</Option>
+                    <Option value="2">Action 2 </Option>
+                    <Option value="3">Action 3</Option>
+                  </Field>
+                </div>
+              </div>
+              <div>
+                <Label>Skills</Label>
+                <div>
+                  <Field
+                    name="skills"
+                    id="skills"
+                    component={Dropdown}
+                    multiselect={true}
+                    placeholder="Select"
+                  >
+                    <Option value="1">.Net Core</Option>
+                    <Option value="2">React js </Option>
+                    <Option value="3">Next js</Option>
+                    <Option value="4">Angular js</Option>
+                  </Field>
+                </div>
+              </div>
+              <Field
+                name="isActive"
+                id="isActive"
+                component={CheckboxField}
+                label="Is Active"
+                type="checkbox"
+              />
+
+              <div>
+                <Label htmlFor="joiningDate">Joining Date</Label>
+                <div>
+                  <Field
+                    name="joiningDate"
+                    id="joiningDate"
+                    component={DatePicker}
+                    placeholder="Joining Date"
+                    maxDate={new Date()}
+                    isMonthPickerVisible={false}
+                  />
+                </div>
+              </div>
+
+              <div>
+                <Label>Notes</Label>
+                <div>
+                  <Field name="notes" id="notes" component={Textarea} />
+                </div>
+              </div>
+              <div className="flex space-x-2">
+                <Button type="submit" disabled={pristine || submitting} appearance="primary">
+                  Submit
+                </Button>
+                <Button
+                  type="button"
+                  //disabled={pristine || submitting}
+
+                  appearance="secondary"
+                >
+                  Clear Values
+                </Button>
+              </div>
             </div>
-          </div>
-          <div className="flex space-x-2">
-            <Button type="submit" disabled={pristine || submitting} appearance="primary">
-              Submit
-            </Button>
-            <Button
-              type="button"
-              //disabled={pristine || submitting}
-              onClick={reset}
-              appearance="secondary"
-            >
-              Clear Values
-            </Button>
-          </div>
-        </div>
-      </form>
+          </form>
+        )}
+      />
+
       <div className="mt-5">{`You submitted:\n\n${result && JSON.stringify(result, null, 2)}`}</div>
     </div>
   );
 };
 
-export default reduxForm({
-  form: 'employeeForm', // a unique identifier for this form
-})(ReduxForm);
+export default ReduxForm;
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,13 +2,11 @@ import { createStore, combineReducers } from 'redux';
 import { counterReducer } from './counterReducer';
 import { themeReducer } from './themeReducer';
 import { navbarReducer } from './navBarReducer';
-import { reducer as reduxFormReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
   counter: counterReducer,
   theme: themeReducer,
   navbar: navbarReducer,
-  form: reduxFormReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
